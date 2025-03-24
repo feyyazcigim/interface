f94c0e0832c7b7f4cc13854bb64ab9e6d89c81e1
Hunter Pickett <hunter@hey.com>
2025-03-24 12:15:41 +0900

    Fix hide column dropdown width

--- src/components/HideColumnDropdown.tsx
+++ src/components/HideColumnDropdown.tsx
@@ -25,9 +25,9 @@ export const HideColumnDropdown = ({ seasonColumns, hiddenFields, toggleColumn, 
     }
 
     const trigger = (
-        <Button className="justify-between sm:h-6 h-8 rounded-full" variant="outline">
+        <Button className="justify-between sm:h-6 h-8 rounded-full w-[140px]" variant="outline">
             <IconImage className="mr-2" src={eyeballCrossed} size={4} />
-            {columnDropdownLabel}
+            <span className="w-full text-center">{columnDropdownLabel}</span>
         </Button>
     )
 
