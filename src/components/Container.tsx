06f8a83ea68110716f10f0c8e4663b3dae372e45
burr <burr.nim@proton.me>
2025-03-25 13:02:08 -0600

    feat: create row + col container components

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,16 +1,28 @@
 import { cn } from "@/utils/utils";
-import { HTMLAttributes, forwardRef } from "react";
+import React from "react";
 
-export type InlineCenterSpanProps = HTMLAttributes<HTMLSpanElement> & {
+export type InlineCenterSpanProps = React.HTMLAttributes<HTMLSpanElement> & {
   gap1?: boolean;
 };
 
-export const InlineCenterSpan = forwardRef<HTMLSpanElement, InlineCenterSpanProps>(
+export const InlineCenterSpan = React.forwardRef<HTMLSpanElement, InlineCenterSpanProps>(
   ({ children, gap1 = false, ...props }, ref) => {
     return (
       <span {...props} ref={ref} className={cn("inline-flex items-center", gap1 && "gap-1", props.className)}>
         {children}
       </span>
     );
+  },
+);
+
+export const Row = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return <div {...props} ref={ref} className={cn("flex flex-row items-center", className)} />;
+  },
+);
+
+export const Col = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return <div {...props} ref={ref} className={cn("flex flex-col", className)} />;
   },
 );
