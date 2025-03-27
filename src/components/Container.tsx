739a41a46a6b93fe1474f10a91da7baaa8abd81f
burr <burr.nim@proton.me>
2025-03-26 18:28:29 -0600

    feat: update react imports

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,11 @@
 import { cn } from "@/utils/utils";
-import React from "react";
+import { forwardRef, HTMLAttributes } from "react";
 
-export type InlineCenterSpanProps = React.HTMLAttributes<HTMLSpanElement> & {
+export type InlineCenterSpanProps = HTMLAttributes<HTMLSpanElement> & {
   gap1?: boolean;
 };
 
-export const InlineCenterSpan = React.forwardRef<HTMLSpanElement, InlineCenterSpanProps>(
+export const InlineCenterSpan = forwardRef<HTMLSpanElement, InlineCenterSpanProps>(
   ({ children, gap1 = false, ...props }, ref) => {
     return (
       <span {...props} ref={ref} className={cn("inline-flex items-center", gap1 && "gap-1", props.className)}>
@@ -15,13 +15,13 @@ export const InlineCenterSpan = React.forwardRef<HTMLSpanElement, InlineCenterSp
   },
 );
 
-export const Row = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+export const Row = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => {
     return <div {...props} ref={ref} className={cn("flex flex-row items-center", className)} />;
   },
 );
 
-export const Col = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+export const Col = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => {
     return <div {...props} ref={ref} className={cn("flex flex-col", className)} />;
   },
