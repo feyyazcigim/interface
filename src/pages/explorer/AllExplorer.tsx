d2930d559b96a6d08a355a57488f5059380b3a60
JOHN DEERE <176664213+JOHNDEERE420@users.noreply.github.com>
2025-04-06 20:54:44 -0300

    explorer page

--- src/pages/explorer/AllExplorer.tsx
+++ src/pages/explorer/AllExplorer.tsx
@@ -1,4 +1,12 @@
+import { AdvancedChart } from "@/components/charts/AdvancedChart";
+import TVChart from "@/components/charts/TVChart";
+import { AdvancedFieldDocument } from "@/generated/gql/graphql";
+
 const AllExplorer = () => {
-  return <>Coming Soon!</>;
+  return (
+    <div className="-mb-4 sm:-mb-20 h-full flex flex-col gap-4 sm:gap-8">
+      <AdvancedChart />
+    </div>
+  );
 };
 export default AllExplorer;
