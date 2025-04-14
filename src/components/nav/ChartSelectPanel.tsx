f37beca87f2016b8d9139a382508a26f7830a9c8
JOHN DEERE <176664213+JOHNDEERE420@users.noreply.github.com>
2025-04-14 00:34:50 -0300

    spacing tweak in chart select panel

--- src/components/nav/ChartSelectPanel.tsx
+++ src/components/nav/ChartSelectPanel.tsx
@@ -104,7 +104,7 @@ const ChartSelectPanel = memo(() => {
                 <div
                   key={`chartSelectList${data.id}`}
                   className={cn(
-                    "flex flex-row items-center gap-3 justify-between hover:cursor-pointer hover:bg-pinto-gray-2/50 py-2 px-6 transition-colors",
+                    "flex flex-row items-center gap-3 my-0.5 justify-between hover:cursor-pointer hover:bg-pinto-gray-2/50 py-2 px-6 transition-colors",
                     isSelected && "bg-pinto-gray-1",
                   )}
                   onClick={() => handleSelection(data.index)}
