6bec27cfcf25e88a1400592d633c3609d9b97a5e
JOHN DEERE <176664213+JOHNDEERE420@users.noreply.github.com>
2025-04-25 10:34:11 -0300

    bounty styling

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,8 +2,8 @@ import PintoLogo from "@/assets/protocol/PintoLogo.svg";
 import PintoLogoText from "@/assets/protocol/PintoLogoText.svg";
 import PintoTokenLogo from "@/assets/tokens/PINTO.png";
 import { useAverageBDVWeightedSiloAPYs } from "@/state/useSiloAPYs";
-import NumberFlow from "@number-flow/react";
-import { useEffect, useState } from "react";
+import NumberFlow, { continuous } from "@number-flow/react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "../components/ui/Button";
 
 function MainCTA() {
@@ -93,10 +93,58 @@ function AuditMarquee() {
 }
 
 function BugBounty() {
+  const [value, setValue] = useState(0o7);
+  const componentRef = useRef(null);
+  const valueChanged = useRef(false);
+
+  useEffect(() => {
+    // Create Intersection Observer to detect when component is visible
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Check if component is intersecting (visible) and animation hasn't started yet
+        if (entry.isIntersecting && !valueChanged.current) {
+          // Mark animation as started to prevent repeating
+          valueChanged.current = true;
+
+          setValue(1200000);
+        }
+      },
+      {
+        root: null, // Use viewport as root
+        threshold: 0.1, // Trigger when at least 10% of component is visible
+      },
+    );
+
+    if (componentRef.current) {
+      observer.observe(componentRef.current);
+    }
+
+    return () => {
+      if (componentRef.current) {
+        observer.unobserve(componentRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className="flex flex-row gap-4">
-      <img src={PintoTokenLogo} alt={"Pinto Token Icon"} />
-      <NumberFlow value={1200000} format={{ style: "currency", currency: "USD", trailingZeroDisplay: "auto" }} />
+    <div className="flex flex-col items-center">
+      <div className="flex flex-row gap-8 items-center" ref={componentRef}>
+        <img src={PintoTokenLogo} alt={"Pinto Token Icon"} className="w-32 h-32" />
+        <NumberFlow
+          value={value}
+          respectMotionPreference={false}
+          trend={+1}
+          format={{ style: "decimal", minimumFractionDigits: 2, maximumFractionDigits: 2, trailingZeroDisplay: "auto" }}
+          className="text-black font-thin text-[8rem]"
+        />
+      </div>
+      <div className="pinto-h2 font-light pt-6">Bug Bounty through Immunefi</div>
+      <div className="flex flex-row gap-4 pt-9">
+        <Button rounded="full">Get Started</Button>
+        <Button variant="outline" rounded="full" className="shadow-none text-pinto-gray-4">
+          Read about the Bug Bounty Program
+        </Button>
+      </div>
     </div>
   );
 }
