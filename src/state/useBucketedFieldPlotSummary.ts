ad74ed19265fc371c22300a6208e0c173eb490d2
burr <burr.nim@proton.me>
2025-05-01 23:42:45 +0700

    feat: integrate field/plot-summary endpoint

---
+++ src/state/useBucketedFieldPlotSummary.ts
@@ -0,0 +1,109 @@
+import { TV } from "@/classes/TokenValue";
+import { API_SERVICES } from "@/constants/endpoints";
+import { PODS } from "@/constants/internalTokens";
+import { defaultQuerySettings } from "@/constants/query";
+import { MAIN_TOKEN } from "@/constants/tokens";
+import { useChainConstant } from "@/utils/chain";
+import { Prettify } from "@/utils/types.generic";
+import { safeJSONStringify } from "@/utils/utils";
+import { useQuery } from "@tanstack/react-query";
+import { useCallback } from "react";
+
+export type FieldPlotSummaryParams<T extends string | number = number> = {
+  bucketSize?: T;
+  onlyHarvested?: boolean;
+  onlyUnharvested?: boolean;
+};
+
+type BaseFieldPlotSummaryResponse = {
+  startSeason: number;
+  numPlots: number;
+  endSeason: number;
+};
+
+type RawFieldPlotBucketSummary = Prettify<
+  {
+    startTimestamp: string;
+    startIndex: string;
+    endIndex: string;
+    avgSownBeansPerPod: number;
+    endTimestamp: string;
+  } & BaseFieldPlotSummaryResponse
+>;
+
+export type FieldPlotBucketSummary = Prettify<
+  {
+    startTimestamp: number;
+    endTimestamp: number;
+    bucketSize: number;
+    startIndex: TV;
+    endIndex: TV;
+    avgSownBeansPerPod: TV;
+    avgTemperature: TV;
+    bucketIndex: number;
+  } & BaseFieldPlotSummaryResponse
+>;
+
+const endpoint = API_SERVICES.pinto;
+const DEFAULT_BUCKET_SIZE = 50_000;
+
+const defaultArgs: FieldPlotSummaryParams<string> = {
+  bucketSize: DEFAULT_BUCKET_SIZE.toString(),
+  onlyHarvested: false,
+  onlyUnharvested: false,
+} as const;
+
+const requestArgs = {
+  method: "GET",
+  headers: new Headers({
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  }),
+} as const;
+
+const combineURLParams = (params?: FieldPlotSummaryParams) => ({ ...defaultArgs, ...params });
+
+const makeRequest = async (params: string, signal: AbortSignal): Promise<RawFieldPlotBucketSummary[]> => {
+  const url = `${endpoint}/field-plot-summary?${params}`;
+  return fetch(url, { signal, ...requestArgs }).then((r) => r.json());
+};
+
+export default function useBucketedFieldPlotSummary(args: FieldPlotSummaryParams = {}) {
+  // Hooks
+  const mainToken = useChainConstant(MAIN_TOKEN);
+
+  // Transform
+  const transform = useCallback(
+    (data: RawFieldPlotBucketSummary[]) => {
+      return data.map((r, i) => {
+        const startIndex = TV.fromBlockchain(r.startIndex, PODS.decimals);
+        const endIndex = TV.fromBlockchain(r.endIndex, PODS.decimals);
+        const avgSownBeansPerPod = TV.fromBlockchain(r.avgSownBeansPerPod, mainToken.decimals);
+        const avgTemperature = TV.fromHuman(1, avgSownBeansPerPod.decimals).div(avgSownBeansPerPod).sub(1).mul(100);
+
+        return {
+          ...r,
+          bucketIndex: i,
+          bucketSize: args.bucketSize ?? DEFAULT_BUCKET_SIZE,
+          startTimestamp: new Date(r.startTimestamp).getTime(),
+          endTimestamp: new Date(r.endTimestamp).getTime(),
+          startIndex,
+          endIndex,
+          avgSownBeansPerPod,
+          avgTemperature,
+        };
+      });
+    },
+    [args.bucketSize, mainToken],
+  );
+
+  // Query
+  const params = safeJSONStringify(combineURLParams(args), undefined);
+
+  return useQuery({
+    queryKey: ["fieldPlotSummary"],
+    queryFn: async ({ signal }) => makeRequest(params, signal),
+    select: transform,
+    ...defaultQuerySettings,
+  });
+}
