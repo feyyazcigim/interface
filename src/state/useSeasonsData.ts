a8ec6aa971987221e3831f933f78daacfd2258fe
Hunter Pickett <hunter@hey.com>
2025-05-14 07:59:10 +0900

    Add back sunrise block to seasonsData

--- src/state/useSeasonsData.ts
+++ src/state/useSeasonsData.ts
@@ -298,6 +298,7 @@ export default function useSeasonsData(
         if (!allData.season) {
           allData.season = currStalkSeasons.season;
           allData.timestamp = Number(currStalkSeasons.createdAt || 0);
+          allData.sunriseBlock = Number(currStalkSeasons.sunriseBlock || 0);
         }
       }
 
