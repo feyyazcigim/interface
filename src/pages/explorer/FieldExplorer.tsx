2b2f4783934e49080b0cedf33136c8820d4f63ea
burr <burr.nim@proton.me>
2025-06-10 17:00:13 +0900

    feat: update Field Explorer

--- src/pages/explorer/FieldExplorer.tsx
+++ src/pages/explorer/FieldExplorer.tsx
@@ -9,23 +9,11 @@ import {
 } from "@/state/seasonal/seasonalDataHooks";
 import { useSunData } from "@/state/useSunData";
 import { chartFormatters as f } from "@/utils/format";
-import { useState } from "react";
+import React, { useState } from "react";
 import FieldTemperatureBarChart from "../field/FieldTemperatureBarChart";
 
 const FieldExplorer = () => {
-  const [podRateTab, setPodRateTab] = useState(TimeTab.Week);
-  const [tempTab, setTempTab] = useState(TimeTab.Week);
-  const [podlineTab, setPodlineTab] = useState(TimeTab.Week);
-  const [sownTab, setSownTab] = useState(TimeTab.Week);
-  const [harvestedTab, setHarvestedTab] = useState(TimeTab.Week);
-
   const season = useSunData().current;
-
-  const podRateData = useSeasonalPodRate(Math.max(0, season - tabToSeasonalLookback(podRateTab)), season);
-  const tempData = useSeasonalTemperature(Math.max(0, season - tabToSeasonalLookback(tempTab)), season);
-  const podIndexData = useSeasonalPodLine(Math.max(0, season - tabToSeasonalLookback(podlineTab)), season);
-  const sownData = useSeasonalSownPinto(Math.max(0, season - tabToSeasonalLookback(sownTab)), season);
-  const harvestData = useSeasonalPodsHarvested(Math.max(0, season - tabToSeasonalLookback(harvestedTab)), season);
 
   return (
     <>
@@ -34,75 +22,131 @@ const FieldExplorer = () => {
           <FieldTemperatureBarChart variant="explorer" />
         </div>
         <div className="w-full sm:w-1/2">
-          <SeasonalChart
-            title="Pod Rate"
-            tooltip="The ratio of Unharvestable Pods per Pinto. The Pod Rate is used by Pinto as a proxy for its health."
-            size="small"
-            fillArea
-            activeTab={podRateTab}
-            onChangeTab={setPodRateTab}
-            useSeasonalResult={podRateData}
-            valueFormatter={f.percent2dFormatter}
-            tickValueFormatter={f.percent0dFormatter}
-          />
+          <PodRateChart season={season} />
         </div>
       </div>
       <div className="flex flex-col sm:flex-row w-full sm:space-x-8">
         <div className="w-full sm:w-1/2">
-          <SeasonalChart
-            title="Max Temperature"
-            tooltip="The maximum interest rate for Sowing Pinto."
-            size="small"
-            activeTab={tempTab}
-            onChangeTab={setTempTab}
-            useSeasonalResult={tempData}
-            valueFormatter={f.percent2dFormatter}
-            tickValueFormatter={f.percent0dFormatter}
-          />
+          <MaxTempChart season={season} />
         </div>
         <div className="w-full sm:w-1/2">
-          <SeasonalChart
-            title="Pod Line"
-            tooltip="The total number of Unharvestable Pods."
-            size="small"
-            fillArea
-            activeTab={podlineTab}
-            onChangeTab={setPodlineTab}
-            useSeasonalResult={podIndexData}
-            valueFormatter={f.number0dFormatter}
-            tickValueFormatter={f.largeNumberFormatter}
-          />
+          <PodLineChart season={season} />
         </div>
       </div>
       <div className="flex flex-col sm:flex-row w-full sm:space-x-8">
         <div className="w-full sm:w-1/2">
-          <SeasonalChart
-            title="Sown Pinto"
-            tooltip="The total number of Pinto Sown."
-            size="small"
-            fillArea
-            activeTab={sownTab}
-            onChangeTab={setSownTab}
-            useSeasonalResult={sownData}
-            valueFormatter={f.number0dFormatter}
-            tickValueFormatter={f.largeNumberFormatter}
-          />
+          <SownPintoChart season={season} />
         </div>
         <div className="w-full sm:w-1/2">
-          <SeasonalChart
-            title="Pods Harvested"
-            tooltip="The total number of Pods Harvested."
-            size="small"
-            fillArea
-            activeTab={harvestedTab}
-            onChangeTab={setHarvestedTab}
-            useSeasonalResult={harvestData}
-            valueFormatter={f.number0dFormatter}
-            tickValueFormatter={f.largeNumberFormatter}
-          />
+          <PodsHarvestedChart season={season} />
         </div>
       </div>
     </>
   );
 };
 export default FieldExplorer;
+
+interface ISeason {
+  season: number;
+}
+
+const useTimeTabs = () => useState(TimeTab.Week);
+
+const PodRateChart = React.memo(({ season }: ISeason) => {
+  const [podRateTab, setPodRateTab] = useTimeTabs();
+
+  const podRateData = useSeasonalPodRate(Math.max(0, season - tabToSeasonalLookback(podRateTab)), season);
+
+  return (
+    <SeasonalChart
+      title="Pod Rate"
+      tooltip="The ratio of Unharvestable Pods per Pinto. The Pod Rate is used by Pinto as a proxy for its health."
+      size="small"
+      fillArea
+      activeTab={podRateTab}
+      onChangeTab={setPodRateTab}
+      useSeasonalResult={podRateData}
+      valueFormatter={f.percent2dFormatter}
+      tickValueFormatter={f.percent0dFormatter}
+    />
+  );
+});
+
+const MaxTempChart = React.memo(({ season }: ISeason) => {
+  const [tempTab, setTempTab] = useTimeTabs();
+
+  const tempData = useSeasonalTemperature(Math.max(0, season - tabToSeasonalLookback(tempTab)), season);
+
+  return (
+    <SeasonalChart
+      title="Max Temperature"
+      tooltip="The maximum interest rate for Sowing Pinto."
+      size="small"
+      activeTab={tempTab}
+      onChangeTab={setTempTab}
+      useSeasonalResult={tempData}
+      valueFormatter={f.percent2dFormatter}
+      tickValueFormatter={f.percent0dFormatter}
+    />
+  );
+});
+
+const PodLineChart = React.memo(({ season }: ISeason) => {
+  const [podlineTab, setPodlineTab] = useTimeTabs();
+
+  const podIndexData = useSeasonalPodLine(Math.max(0, season - tabToSeasonalLookback(podlineTab)), season);
+
+  return (
+    <SeasonalChart
+      title="Pod Line"
+      tooltip="The total number of Unharvestable Pods."
+      size="small"
+      fillArea
+      activeTab={podlineTab}
+      onChangeTab={setPodlineTab}
+      useSeasonalResult={podIndexData}
+      valueFormatter={f.number0dFormatter}
+      tickValueFormatter={f.largeNumberFormatter}
+    />
+  );
+});
+
+const SownPintoChart = React.memo(({ season }: ISeason) => {
+  const [sownTab, setSownTab] = useTimeTabs();
+
+  const sownData = useSeasonalSownPinto(Math.max(0, season - tabToSeasonalLookback(sownTab)), season);
+
+  return (
+    <SeasonalChart
+      title="Sown Pinto"
+      tooltip="The total number of Pinto Sown."
+      size="small"
+      fillArea
+      activeTab={sownTab}
+      onChangeTab={setSownTab}
+      useSeasonalResult={sownData}
+      valueFormatter={f.number0dFormatter}
+      tickValueFormatter={f.largeNumberFormatter}
+    />
+  );
+});
+
+const PodsHarvestedChart = React.memo(({ season }: ISeason) => {
+  const [harvestedTab, setHarvestedTab] = useTimeTabs();
+
+  const harvestData = useSeasonalPodsHarvested(Math.max(0, season - tabToSeasonalLookback(harvestedTab)), season);
+
+  return (
+    <SeasonalChart
+      title="Pods Harvested"
+      tooltip="The total number of Pods Harvested."
+      size="small"
+      fillArea
+      activeTab={harvestedTab}
+      onChangeTab={setHarvestedTab}
+      useSeasonalResult={harvestData}
+      valueFormatter={f.number0dFormatter}
+      tickValueFormatter={f.largeNumberFormatter}
+    />
+  );
+});
