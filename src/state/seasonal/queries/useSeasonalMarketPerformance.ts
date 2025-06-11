eb279d0fef81f006a915c8d48d80357cfa4f4b19
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-06-11 16:02:03 -0400

    Manual accumulation from starting season

--- src/state/seasonal/queries/useSeasonalMarketPerformance.ts
+++ src/state/seasonal/queries/useSeasonalMarketPerformance.ts
@@ -17,12 +17,28 @@ export enum SMPChartType {
   USD_CUMULATIVE = 2,
   PERCENT_CUMULATIVE = 3,
 }
+
+// To be accessed as CHART_FIELDS[SMPChartType % 2]
 const CHART_FIELDS = [
   ["usdChange", "totalUsdChange"],
   ["percentChange", "totalPercentChange"],
-  ["cumulativeUsdChange", "cumulativeTotalUsdChange"],
-  ["cumulativePercentChange", "cumulativeTotalPercentChange"],
 ];
+
+const accumulateUsd = (prev: number, curr: number): number => {
+  return prev + curr;
+};
+const accumulatePercent = (prev: number, curr: number): number => {
+  return (prev + 1) * (curr + 1) - 1;
+};
+
+// Manually accumulate values to allow for arbitrary starting point; subgraph cumulative values are since deployment
+const accumulator = (chartType: SMPChartType): ((prev: number, curr: number) => number) => {
+  if (chartType === SMPChartType.USD_CUMULATIVE) {
+    return accumulateUsd;
+  } else {
+    return accumulatePercent;
+  }
+};
 
 const paginateSettings: PaginationSettings<
   MarketPerformanceSeasonal,
@@ -93,16 +109,33 @@ export function useSeasonalMarketPerformance(
         }
 
         responseData[underlyingToken.symbol] ??= [];
-        responseData[underlyingToken.symbol].push({
+        const arr = responseData[underlyingToken.symbol];
+
+        const value =
+          chartType < SMPChartType.USD_CUMULATIVE
+            ? Number(season[CHART_FIELDS[chartType % 2][0]][tokenIdx])
+            : accumulator(chartType)(
+                arr[arr.length - 1]?.value ?? 0,
+                Number(season[CHART_FIELDS[chartType % 2][0]][tokenIdx]),
+              );
+        arr.push({
           season: season.season,
-          value: season[CHART_FIELDS[chartType][0]][tokenIdx],
+          value,
           timestamp: new Date(Number(season.timestamp) * 1000),
         });
+
         ++tokenIdx;
       }
+      const value =
+        chartType < SMPChartType.USD_CUMULATIVE
+          ? Number(season[CHART_FIELDS[chartType % 2][1]])
+          : accumulator(chartType)(
+              responseData.NET[responseData.NET.length - 1]?.value ?? 0,
+              Number(season[CHART_FIELDS[chartType % 2][1]]),
+            );
       responseData.NET.push({
         season: season.season,
-        value: season[CHART_FIELDS[chartType][1]],
+        value,
         timestamp: new Date(Number(season.timestamp) * 1000),
       });
     }
