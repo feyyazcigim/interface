c00c7625d233a65b80af9dac4fb4844518da8b04
JOHN DEERE <176664213+JOHNDEERE420@users.noreply.github.com>
2025-09-09 14:26:57 -0300

    rounded beaver icon

--- src/components/landing/TxFloater.tsx
+++ src/components/landing/TxFloater.tsx
@@ -187,7 +187,13 @@ export default function TxFloater({
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
       >
-        <img className="w-8 h-8" src={isFixed ? from : lastValidFromRef.current} alt="Farmer Icon" />
+        <div className="w-8 h-8 rounded-full overflow-hidden flex-shrink-0 flex items-center justify-center">
+          <img
+            className="w-full h-full object-cover"
+            src={isFixed ? from : lastValidFromRef.current}
+            alt="Farmer Icon"
+          />
+        </div>
         <img
           alt={lastValidTxTypeRef.current || "Transaction Type Icon"}
           src={
