ad6f53dadd4aa5ed1d5e486a779fa781cd158521
JOHN DEERE <176664213+JOHNDEERE420@users.noreply.github.com>
2025-09-11 08:20:37 -0300

    spacing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -181,10 +181,10 @@ export default function Landing() {
         <section className="flex flex-col overflow-clip place-content-center sm:w-full min-h-[50rem] sm:min-h-[max(800px,100vh)] bg-[linear-gradient(180deg,#FEFDF7_-0.11%,#ECF7ED_49.41%,#FEFDF6_99.89%)]">
           <ProjectStats />
         </section>
-        <section className="flex flex-col overflow-clip place-content-center sm:w-full min-h-screen bg-[linear-gradient(180deg,#FEFDF7_-0.11%,#D8F1E2_49.41%,#FEFDF6_99.89%)]">
+        <section className="flex flex-col overflow-clip place-content-center sm:w-full min-h-[34rem] bg-[linear-gradient(180deg,#FEFDF7_-0.11%,#D8F1E2_49.41%,#FEFDF6_99.89%)]">
           <BugBounty />
         </section>
-        <section className="flex flex-col overflow-clip place-content-center h-auto min-h-[110rem] w-full sm:h-screen sm:min-h-[max(800px,100vh)] bg-[linear-gradient(180deg,#FEFDF7_-0.11%,#D8F1E2_49.41%,#FEFDF6_99.89%)]">
+        <section className="flex flex-col overflow-clip place-content-center h-auto min-h-[114rem] w-full sm:h-screen sm:min-h-[max(800px,100vh)] bg-[linear-gradient(180deg,#FEFDF7_-0.11%,#D8F1E2_49.41%,#FEFDF6_99.89%)]">
           <Resources />
         </section>
       </div>
