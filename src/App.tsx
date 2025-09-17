8c717cf4752e1e83e73c156441b20995dab82532
JOHN DEERE <176664213+JOHNDEERE420@users.noreply.github.com>
2025-09-17 18:44:41 -0300

    smarter chunking strategy

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,11 @@ import PageMetaWrapper from "./components/PageMetaWrapper";
 import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/nav/nav/Navbar";
 import { externalLinks } from "./constants/links";
-import Collection from "./pages/Collection";
-import Error404 from "./pages/Error404";
-import Explorer from "./pages/Explorer";
-import Field from "./pages/Field";
-import { Market as MarketPage } from "./pages/Market";
-import Overview from "./pages/Overview";
-import Silo from "./pages/Silo";
-import SiloToken from "./pages/SiloToken";
-import Swap from "./pages/Swap";
-import Transfer from "./pages/Transfer";
-import Whitepaper from "./pages/Whitepaper";
-import NewUserView from "./pages/overview/NewUserView";
 
+// Create two main chunks: Landing page and the rest of the app
 const Landing = lazy(() => import("./pages/Landing"));
+const ProtectedLayoutLazy = lazy(() => import("./ProtectedLayout"));
+const Whitepaper = lazy(() => import("./pages/Whitepaper"));
 
 import Footer from "@/components/Footer";
 import { MobileActionBarProvider } from "@/components/MobileActionBarContext";
@@ -48,137 +39,6 @@ function AppLayout({ children }) {
   );
 }
 
-function ProtectedLayout() {
-  return (
-    <Routes>
-      <Route
-        path="/overview"
-        element={
-          <PageMetaWrapper metaKey="overview">
-            <Overview />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/how-pinto-works"
-        element={
-          <PageMetaWrapper metaKey="overview">
-            <NewUserView />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/silo"
-        element={
-          <PageMetaWrapper metaKey="silo">
-            <Silo />
-          </PageMetaWrapper>
-        }
-      />
-      <Route path="/silo/:tokenAddress" element={<SiloToken />} />
-      <Route path="/sPinto" element={<SiloToken />} />
-      <Route path="/wrap" element={<Navigate to="/sPinto" replace />} />
-      <Route
-        path="/field"
-        element={
-          <PageMetaWrapper metaKey="field">
-            <Field />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/swap"
-        element={
-          <PageMetaWrapper metaKey="swap">
-            <Swap />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/market/pods"
-        element={
-          <PageMetaWrapper metaKey="market">
-            <MarketPage />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/market/pods/:mode"
-        element={
-          <PageMetaWrapper metaKey="market">
-            <MarketPage />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/market/pods/:mode/:id"
-        element={
-          <PageMetaWrapper metaKey="market">
-            <MarketPage />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/transfer"
-        element={
-          <PageMetaWrapper metaKey="transfer">
-            <Transfer />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/transfer/:mode"
-        element={
-          <PageMetaWrapper metaKey="transfer">
-            <Transfer />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/explorer/"
-        element={
-          <PageMetaWrapper metaKey="explorer">
-            <Explorer />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/explorer/:tab"
-        element={
-          <PageMetaWrapper metaKey="explorer">
-            <Explorer />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/collection"
-        element={
-          <PageMetaWrapper metaKey="nftCollection">
-            <Collection />
-          </PageMetaWrapper>
-        }
-      />
-      <Route
-        path="/404"
-        element={
-          <PageMetaWrapper metaKey="404">
-            <Error404 />
-          </PageMetaWrapper>
-        }
-      />
-      {isDev() && <Route path="/dev" element={<DevPage />} />}
-      <Route
-        path="*"
-        element={
-          <PageMetaWrapper metaKey="404">
-            <Error404 />
-          </PageMetaWrapper>
-        }
-      />
-    </Routes>
-  );
-}
-
 function App() {
   useMetaCRM();
 
@@ -202,15 +62,21 @@ function App() {
               }
             />
             <Route
-              path="/how-pinto-works"
+              path="/whitepaper"
               element={
-                <PageMetaWrapper metaKey="overview">
-                  <NewUserView />
-                </PageMetaWrapper>
+                <Suspense fallback={<div className="h-screen w-screen" />}>
+                  <Whitepaper />
+                </Suspense>
               }
             />
-            <Route path="/whitepaper" element={<Whitepaper />} />
-            <Route path="/*" element={<ProtectedLayout />} />
+            <Route
+              path="/*"
+              element={
+                <Suspense fallback={<div className="h-screen w-screen" />}>
+                  <ProtectedLayoutLazy />
+                </Suspense>
+              }
+            />
             <Route
               path="/announcing-pinto"
               Component={() => {
