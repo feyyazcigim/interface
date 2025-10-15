ed3b64d72d9baae610763f362c54934b609cbbf4
JOHN DEERE <176664213+JOHNDEERE420@users.noreply.github.com>
2025-10-15 09:28:07 -0300

    Add comment
    Added a conditional comment and logic to display the TimeTabsSelector component only when the selected tab index is not 6, ensuring the time selector is hidden on the 'All' tab.

--- src/pages/Explorer.tsx
+++ src/pages/Explorer.tsx
@@ -84,6 +84,7 @@ const Explorer = () => {
           <div className="flex flex-col gap-2 sm:ml-4">
             <div className="flex justify-between items-center">
               <div className="pinto-h2 sm:pinto-h1 ml-[-3px]">Data</div>
+              {/* Only show time selector when not on "All" tab */}
               {selectedIdx !== 6 && (
                 <div className="scale-110 sm:mr-4">
                   <TimeTabsSelector
